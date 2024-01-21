9e9a52c48484458b9d8c065294bf48cd0628b061
NicoElbers <Ryojipog@gmail.com>
2024-01-21 17:10:21 +0100

    files that sould be removed I hope

--- target/debug/deps/raytracing_in_a_weekend-026102261f8fe67b.d
+++ target/debug/deps/raytracing_in_a_weekend-026102261f8fe67b.d
@@ -1,12 +1,15 @@
-/home/nico/personal/rust/raytracing_in_a_weekend/target/debug/deps/libraytracing_in_a_weekend-026102261f8fe67b.rmeta: src/main.rs src/color.rs src/ray.rs src/vec3.rs Cargo.toml
+/home/nico/personal/rust/raytracing_in_a_weekend/target/debug/deps/libraytracing_in_a_weekend-026102261f8fe67b.rmeta: src/main.rs src/color.rs src/hittable.rs src/point3.rs src/ray.rs src/shapes.rs src/vec3.rs Cargo.toml
 
-/home/nico/personal/rust/raytracing_in_a_weekend/target/debug/deps/raytracing_in_a_weekend-026102261f8fe67b.d: src/main.rs src/color.rs src/ray.rs src/vec3.rs Cargo.toml
+/home/nico/personal/rust/raytracing_in_a_weekend/target/debug/deps/raytracing_in_a_weekend-026102261f8fe67b.d: src/main.rs src/color.rs src/hittable.rs src/point3.rs src/ray.rs src/shapes.rs src/vec3.rs Cargo.toml
 
 src/main.rs:
 src/color.rs:
+src/hittable.rs:
+src/point3.rs:
 src/ray.rs:
+src/shapes.rs:
 src/vec3.rs:
 Cargo.toml:
 
-# env-dep:CLIPPY_ARGS=--no-deps__CLIPPY_HACKERY__-Dclippy::correctness__CLIPPY_HACKERY__-Dclippy::complexity__CLIPPY_HACKERY__-Wclippy::perf__CLIPPY_HACKERY__-Wclippy::pedantic__CLIPPY_HACKERY__
+# env-dep:CLIPPY_ARGS=--no-deps__CLIPPY_HACKERY__-Dclippy::pedantic__CLIPPY_HACKERY__-Dclippy::nursery__CLIPPY_HACKERY__-Dclippy::unwrap_used__CLIPPY_HACKERY__-Dclippy::enum_glob_use__CLIPPY_HACKERY__-Wclippy::complexity__CLIPPY_HACKERY__-Wclippy::perf__CLIPPY_HACKERY__-Aclippy::suboptimal_flops__CLIPPY_HACKERY__
 # env-dep:CLIPPY_CONF_DIR
