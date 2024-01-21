2ce7960ecc1640dc2d273826d827ec6100080df5
NicoElbers <Ryojipog@gmail.com>
2024-01-21 22:23:33 +0100

    feat: implement display for color

--- src/color.rs
+++ src/color.rs
@@ -1,4 +1,5 @@
 use std::{
+    fmt::Display,
     fs::File,
     io::Write,
     ops::{Add, Div, Mul},
@@ -65,6 +66,12 @@ impl Div<f64> for Color {
             g: self.g / scalar,
             b: self.b / scalar,
         }
+    }
+}
+
+impl Display for Color {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} {} {}", self.r, self.g, self.b)
     }
 }
 
