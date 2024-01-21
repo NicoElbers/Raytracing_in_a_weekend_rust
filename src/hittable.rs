8f2dbdd771b2ce3c84fe1dcdbbb95b53b58755d8
NicoElbers <Ryojipog@gmail.com>
2024-01-21 22:25:10 +0100

    feat: hittable things

--- src/hittable.rs
+++ src/hittable.rs
@@ -1,15 +1,96 @@
-use crate::vec3::Vec3;
-use crate::ray::Ray;
+use std::cmp::Ordering;
+use std::fmt::Debug;
+use std::rc::Rc;
+
 use crate::point3::Point3;
+use crate::ray::Ray;
+use crate::vec3::Vec3;
 
 #[derive(Default, Debug, Clone, Copy)]
 pub struct HitRecord {
-    pub p: Point3,
-    pub normal: Vec3,
-    pub t: f64,
+    point: Point3,
+    normal: Vec3,
+    time: f64,
+    front_face: bool,
 }
 
-trait Hittable {
-    fn new(p: Point3, normal: Vec3, t: f64) -> Self;
-    fn hit(r: &Ray, ray_tmin: f64, rec: HitRecord) -> bool;
+impl HitRecord {
+    pub fn new(point: Point3, normal: Vec3, time: f64, ray: Ray) -> Self {
+        let (front_face, normal) = Self::face_normal(ray, normal);
+
+        Self {
+            point,
+            normal,
+            time,
+            front_face,
+        }
+    }
+
+    pub const fn point(&self) -> Point3 {
+        self.point
+    }
+
+    pub const fn normal(&self) -> Vec3 {
+        self.normal
+    }
+
+    pub const fn time(&self) -> f64 {
+        self.time
+    }
+
+    pub const fn front_face(&self) -> bool {
+        self.front_face
+    }
+
+    fn face_normal(r: Ray, outward_normal: Vec3) -> (bool, Vec3) {
+        #[cfg(debug_assertions)]
+        assert!(outward_normal.is_unit());
+
+        let front_face = Vec3::dot(r.dir(), outward_normal) < 0.0;
+
+        if front_face {
+            (true, outward_normal)
+        } else {
+            (false, -outward_normal)
+        }
+    }
+}
+
+pub trait Hittable: Debug {
+    fn hit(&self, r: &Ray, ray_tmin: f64, ray_tmax: f64) -> Option<HitRecord>;
+}
+
+#[derive(Default, Debug, Clone)]
+pub struct List {
+    objects: Vec<Rc<dyn Hittable>>,
+}
+
+impl List {
+    pub fn new() -> Self {
+        Self {
+            objects: Vec::new(),
+        }
+    }
+
+    pub fn objects(&self) -> &Vec<Rc<dyn Hittable>> {
+        &self.objects
+    }
+
+    pub fn clear(&mut self) {
+        self.objects.clear();
+    }
+
+    pub fn add(&mut self, obj: Rc<dyn Hittable>) {
+        self.objects.push(obj);
+    }
+
+    pub fn hit_closest(&self, ray: &Ray, ray_tmin: f64, ray_tmax: f64) -> Option<HitRecord> {
+        self.objects
+            .iter()
+            .filter_map(|obj| obj.hit(ray, ray_tmin, ray_tmax))
+            .min_by(|closest, next| {
+                f64::partial_cmp(&closest.time, &next.time) //
+                    .unwrap_or(Ordering::Equal)
+            })
+    }
 }
